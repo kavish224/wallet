e88e99027a358f98720a07f2ac298c7dd24db139
Kavish Ambani <kavishkkambani@gmail.com>
2024-09-09 10:36:42 +0530

    first commit backend

---
+++ backend/db.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+mongoose.connect("mongodb+srv://kavishfinan:3XjjsKQaTxwGtwXI@cluster0.gd1mq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const accountSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+// userSchema.pre("save", async function (next) {
+//     if (!this.isModified("password")) return next();
+//     this.password = await bcrypt.hash(this.password, 10)
+//     next()
+// })
+
+// userSchema.methods.isPasswordCorrect = async function (password) {
+//     return await bcrypt.compare(password, this.password)
+// }
+const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
+
+module.exports = {
+    User,
+    Account,
+}
---
+++ backend/routes/account.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const zod = require("zod");
+const { User, Account } = require("../db");
+const {JWT_SECRET} = require("../config");
+const jwt = require("jsonwebtoken");
+const { default: mongoose } = require("mongoose");
+const { authMiddleware } = require("../middleware");
+const router = express.Router();
+
+router.get("/balance",authMiddleware, async(req, res) => {
+    const account = await Account.findOne({
+        userId: req.userId
+    });
+    return res.json({
+        balance: account.balance
+    })
+});
+
+router.post("/transfer",authMiddleware, async(req, res) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    const {amount, to} = req.body;
+    const account = await Account.findOne({userId: req.userId}).session(session);
+    if(!account) {
+        await session.abortTransaction();
+        return res.json({
+            message: "account not found"
+        })
+    }
+    if (account.balance < amount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "insufficient balance"
+        })
+    }
+    const toAccount = await Account.findOne({userId: to}).session(session);
+    if(!toAccount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "to account not found"
+        })
+    }
+    await Account.updateOne({userId: req.userId},{$inc: {balance: -amount}}).session(session);
+    await Account.updateOne({userId: to}, {$inc: {balance: amount}}).session(session);
+    await session.commitTransaction();
+    res.json({
+        message: "transfer successful"
+    })
+})
+module.exports = router;
