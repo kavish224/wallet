6eea0ebd0db88f7122ee143bbf374936b9c8741d
Kavish Ambani <kavishkkambani@gmail.com>
2024-10-15 01:10:47 +0530

    add vercel link

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Configure CORS
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://wallet.kavishambani.in'], // Allow requests from this origin
+    origin: ['http://localhost:5173', 'https://wallet.kavishambani.in', 'https://wallet-phi-nine.vercel.app'], // Allow requests from this origin
     methods: "GET,POST,PUT,DELETE,OPTIONS", // Specify allowed methods
     allowedHeaders: "Content-Type,Authorization", // Allow specific headers
     credentials: true // Enable sending of cookies (if needed)
